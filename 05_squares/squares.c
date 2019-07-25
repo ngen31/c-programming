d66d9cabe57827defa979044787fff2ced5d7799
Coursera Learner <nobody@nowhere.nul>
2019-07-25 22:34:21 +0000

    squares.c

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int fMax(int, int);
+int fMin(int, int);
 
 /* 
  * Determines if coord is in range between
@@ -8,8 +9,13 @@
  */
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
+  if((coord >= offset) && (coord < (offset + size))){
+    return 1;
+  }
   // else, return 0
-  return 0;
+  else {
+    return 0;
+  }
 }
 
 /*
@@ -18,36 +24,70 @@ int isInRange(int coord, int offset, int size) {
  */
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
+  if((coord == offset) || (offset + size)){
+    return 1;
+  }
   // return 1, else return 0
+  else {
   return 0;
+  }
 }
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+  int w = fMax(size1, (x_offset + size2));
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h = fMax(size1, (y_offset + size2));
   //count from 0 to h. Call the number you count with y
-
+  for(int y=0;y<h;y++){
     //count from 0 to w. Call the number you count with x
-
+    for(int x=0;x<w;x++){
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
       //  OR
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
+      if(   (  ((x >= x_offset) && (x < (x_offset+size2)) ) && ( (y == y_offset) || (y == (y_offset+size2-1)) )  ) || (  ((y >= y_offset) && (y < (y_offset + size2)) ) && ( (x == x_offset) || (x == (x_offset + size2 - 1)) )  )   ){
       // if so, print a *
-
+	printf("*");
+      }
       //if not,
+      else if(   ((x<size1) && ( (y == 0) || (y == (size1-1)) ) )||((y<size1) && ( (x == 0) || (x == (size1-1)) ) )   ){
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-
+	printf("#");
+      }
       //else print a space
-    //when you finish counting x from 0 to w, 
+      else {
+	printf(" ");
+      }
+    //when you finish counting x from 0 to w,
+    }
     //print a newline
+    printf("\n");
+  }
 
 }
+
+int fMax(int num1, int num2){
+  if(num1 > num2){
+    return num1;
+  }
+  else{
+return num2;
+  }
+}
+
+int fMin(int num1, int num2){
+  if(num1 < num2){
+    return num1;
+  }
+  else{
+    return num2;
+  }
+}
+
